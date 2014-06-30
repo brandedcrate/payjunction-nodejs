222395d9c6d7d49bbced88b4619979bb90726669
Stephen Crosby <stephen@brandedcrate.com>
2014-06-29 22:28:40 -0700

    integration tests for all transaction types

--- test/integration/transaction.js
+++ test/integration/transaction.js
@@ -6,7 +6,7 @@ var randomAmount = function(){
   value = Math.round(value);
   value = value / 100;
   value = value + 5;
-  return value;
+  return value.toFixed(2);
 };
 
 var payjunction = new PayjunctionClient({
@@ -16,15 +16,58 @@ var payjunction = new PayjunctionClient({
   endpoint: 'test'
 });
 
+var isSuccessfulTransaction = function(it) {
+  it('has no errors', function(){
+    assert.equal(this.data.errors, undefined);
+  });
+
+  it('is approved', function(){
+    assert.equal(this.data.response.approved, true);
+  });
+
+  it('status is capture', function(){
+    assert.equal(this.data.status, 'CAPTURE');
+  });
+};
+
+var createTransaction = function(done){
+  var test = this;
+  this.transaction = payjunction.transaction.create({
+    achRoutingNumber: 104000016,
+    achAccountNumber: 123456789,
+    achAccountType: 'CHECKING',
+    achType: 'PPD',
+    amountBase: randomAmount()
+  });
+
+  this.transaction.on('complete', function(data){
+    done(data);
+  });
+};
+
 describe('transactions', function(){
-  describe('creates a transaction', function(done){
+  describe('creates an ACH transaction', function(){
+
+    before(function(done){
+      var test = this;
+
+      createTransaction(function(data){
+        test.data = data;
+        done();
+      });
+    });
+
+    isSuccessfulTransaction(it);
+  });
+
+  describe('creates a credit card transaction (keyed)', function(done){
     before(function(done){
       var test = this;
       this.transaction = payjunction.transaction.create({
-        achRoutingNumber: 104000016,
-        achAccountNumber: 123456789,
-        achAccountType: 'CHECKING',
-        achType: 'PPD',
+        cardNumber: 4444333322221111,
+        cardExpMonth: '01',
+        cardExpYear: '18',
+        cardCvv: 999,
         amountBase: randomAmount()
       });
 
@@ -34,46 +77,42 @@ describe('transactions', function(){
       });
     });
 
-    it('has no errors', function(){
-      assert.equal(this.data.errors, undefined);
-    });
-
-    it('is approved', function(){
-      assert.equal(this.data.response.approved, true);
-    });
-
-    it('status is capture', function(){
-      assert.equal(this.data.status, 'CAPTURE');
-    });
+    isSuccessfulTransaction(it);
   });
 
-  describe('voids a transaction', function(done){
+  describe('creates a credit card transaction (keyed)', function(done){
     before(function(done){
       var test = this;
-      var transaction = payjunction.transaction.create({
-        achRoutingNumber: 104000016,
-        achAccountNumber: 123456789,
-        achAccountType: 'CHECKING',
-        achType: 'PPD',
+      this.transaction = payjunction.transaction.create({
+        cardTrack: '%B4444333322221111^First/Last^1712980100000?;4444333322221111=1712980100000?',
         amountBase: randomAmount()
       });
 
-      transaction.on('complete', function(data){
-        test.transactionId = data.transactionId;
+      this.transaction.on('complete', function(data){
+        test.data = data;
         done();
       });
     });
 
+    isSuccessfulTransaction(it);
+  });
+
+  describe('voids a transaction', function(){
+    this.timeout(6000);
+
     before(function(done){
       var test = this;
-      var transaction = payjunction.transaction.update({
-        id: this.transactionId,
-        status: 'VOID'
-      });
 
-      transaction.on('complete', function(data){
-        test.data = data;
-        done();
+      createTransaction(function(data){
+        var transaction = payjunction.transaction.update({
+          id: data.transactionId,
+          status: 'VOID'
+        });
+
+        transaction.on('complete', function(data){
+          test.data = data;
+          done();
+        });
       });
     });
 
